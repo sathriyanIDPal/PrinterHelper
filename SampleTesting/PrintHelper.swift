3686f43bad939d2739f925bb8fd38b9ed92013e8
sathriyanIDPal <sathriyan.aitech@id-pal.com>
2023-04-12 11:09:52 +0530

    First Commit

---
+++ SampleTesting/PrintHelper.swift
@@ -0,0 +1,14 @@
+//
+//  PrintHelper.swift
+//  SampleTesting
+//
+//  Created by Sathriyan on 12/04/23.
+//
+
+import Foundation
+
+public func helpPrint(message: String) {
+    print("========================================")
+    print(message)
+    print("========================================")
+}
---
+++ SampleTesting/SampleTesting.h
@@ -0,0 +1,18 @@
+//
+//  SampleTesting.h
+//  SampleTesting
+//
+//  Created by Sathriyan on 12/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleTesting.
+FOUNDATION_EXPORT double SampleTestingVersionNumber;
+
+//! Project version string for SampleTesting.
+FOUNDATION_EXPORT const unsigned char SampleTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleTesting/PublicHeader.h>
+
+
